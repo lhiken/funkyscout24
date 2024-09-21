1314f830357df0e65ee874dd451d5745534f9454
Chen <lauchenxia@gmail.com>
2024-09-21 16:48:15 -0700

    added install prompt

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Funkyscout
 
 FRC 846's scouting app for the 2025-2026 season.
+___
+
+#### Preface
+
+Funkyscout uses react with typescript. 
+
+#### Development
+
+For local development, clone the repo and open a terminal window in your funkyscout folder, and run the following commands. Make sure you have node.js installed beforehand.
+
+```
+npm install vite
+npm run dev
+```
--- src/app/routes/auth.tsx
+++ src/app/routes/auth.tsx
@@ -1,7 +1,27 @@
 import { motion } from "framer-motion"
 import './styles/auth.css'
 
+const InstallPrompt = () => {
+   return (
+      <>
+         <div id="install-prompt">
+            <div id="install-prompt-header">
+               <i className="fa-solid fa-xmark" />
+               Uh oh!
+            </div>
+            <div id="install-prompt-details">
+               The app isn't installed! 
+               <div>On Android: Click <i className="fa-solid fa-ellipsis-vertical"/> at the top right &gt; <i className="fa-solid fa-mobile-screen-button"/> Add to Home Screen &gt; <i className="fa-solid fa-circle-arrow-down"/> Install.</div>
+               <div>On iOS: Click <i className="fa-solid fa-arrow-up-from-bracket"/> at the bottom &gt; <i className="fa-regular fa-square-plus"/> Add to Home Screen &gt; <i className="fa-solid fa-circle-arrow-down"/> Add.</div>
+            </div>
+         </div>
+      </>
+   )
+}
+
 const AuthPage = () => {
+   const installed = window.matchMedia('(display-mode: standalone)').matches;
+
    return (
       <>
          <motion.div
@@ -14,18 +34,19 @@ const AuthPage = () => {
             <div id="auth-header">
                funkyscout
             </div>
+            {installed ? null : <InstallPrompt />}
             <div id="auth-box">
                <button id="auth-top">
-                  <i className='fa-solid fa-chevron-down'></i>
+                  <i className='fa-solid fa-chevron-down' />
                   Event
                </button>
                <div id='auth-bottom'>
                   <button id="auth-name">
-                     <i className='fa-solid fa-chevron-down'></i>
+                     <i className='fa-solid fa-chevron-down' />
                      Name
                   </button>
                   <button id="auth-submit">
-                     <i className="fa-solid fa-arrow-right"></i>
+                     <i className="fa-solid fa-arrow-right" />
                   </button>
                </div>
             </div>

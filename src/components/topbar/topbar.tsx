f2dd52ec228aa758589ead38029e714312dde1a8
coconut-z <jiayi09.zhou@gmail.com>
2024-09-22 22:14:37 -0700

    topbar

---
+++ src/components/topbar/topbar.tsx
@@ -0,0 +1,14 @@
+import './topbar.css';
+
+const Topbar = () => {
+
+
+   return (<>
+   <button id="topbar-button">
+    <i className="fa-solid fa-moon"/>
+   </button>
+   </>
+   );
+}
+
+export default Topbar;

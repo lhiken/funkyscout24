522d16a1a794acb9ab27985f1726953fb4a9f986
coconut-z <jiayi09.zhou@gmail.com>
2024-10-10 15:38:49 -0700

    Auto
    Fixed the button onClick detection

--- dev/src/auto/button.ts
+++ dev/src/auto/button.ts
@@ -9,13 +9,16 @@ class Button {
         [90, 200],
         [150, 200],
     ];
+    static color: string = "#CDA745";
+    static colorDim: string = "#302A1B";
 
     num!: number;
     x!: number;
     y!: number;
     width: number = 15;
-    color: string = "#CDA745";
+
     buttonSize: number = 100;
+    isClicked : boolean = false;
 
     constructor(num: number) {
         this.num = num;
@@ -26,16 +29,15 @@ class Button {
     }
 
     draw(ctx: CanvasRenderingContext2D): void {
-        ctx.strokeStyle = this.color;
+        ctx.strokeStyle = Button.color;
+        if (this.isClicked == true ){
+            ctx.strokeStyle = Button.colorDim;
+        }
         ctx.lineWidth = 5;
         ctx.beginPath();
         ctx.arc(this.x, this.y + this.width / 2, this.width, 0, Math.PI * 2);
         ctx.stroke();
     }
 
-    isClicked(x: number, y: number) {
-        return x > this.x - this.buttonSize && x < this.x + this.width + this.buttonSize &&
-            y > this.y - this.buttonSize && y < this.y + this.width + this.buttonSize;
-    }
 }
 export default Button;

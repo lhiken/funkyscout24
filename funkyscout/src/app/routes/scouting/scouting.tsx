05ddf4577b6cc770592a2e5f2431f4fa67cac4bd
coconut-z <jiayi09.zhou@gmail.com>
2024-10-17 20:54:58 -0700

    omg database working (not really)

--- funkyscout/src/app/routes/scouting/scouting.tsx
+++ funkyscout/src/app/routes/scouting/scouting.tsx
@@ -3,6 +3,7 @@ import { getNextMatch } from "../../../utils/database/datacache";
 import { AnimatePresence, motion } from "framer-motion";
 import { Combobox, ComboboxButton, ComboboxInput, ComboboxOption, ComboboxOptions, Field } from "@headlessui/react";
 import './scouting.css';
+import supabase from "../../../utils/database/supabase";
 
 const ScoutingPage = () => {
    const event = localStorage.getItem("event");
@@ -13,8 +14,38 @@ const ScoutingPage = () => {
       alliance: boolean;
       team: number;
    }
-
+   const [matches, setMatches] = useState<ShiftData[]>();
    const [selectedMatch, setSelectedMatch] = useState<ShiftData | null>(null);
+
+
+   const fetchMatches = async () => {
+         if(event && user){
+         const{data:matches,error} = await supabase
+            .from("event_data")
+            .select("match,alliance,team")
+            .eq("event",event)
+            .eq("author",user)
+            .order("match", { ascending: true })
+            if (error) {
+               console.error(error);
+            }
+            console.log(matches);
+            return matches!.map((matches) => ({
+               match: matches.match,
+               alliance: matches.alliance,
+               team :matches.team,
+            }))
+      }
+   }
+   
+   const handleMatchSelection = () => {
+      setMatches(undefined);
+      fetchMatches()
+         .then((res) => setMatches(res))
+         .catch((err) => console.error(err));
+      console.log(matches)
+   };
+
 
    const autofillData = () => {
       getNextMatch(event!, user!).then((res) => {
@@ -51,7 +82,7 @@ const ScoutingPage = () => {
                   >
                      {({ open }) => (
                         <>
-                           <ComboboxButton >
+                           <ComboboxButton onClick ={handleMatchSelection}>
                               <i className="fa-solid fa-chevron-down" />
                            </ComboboxButton>
                            <ComboboxInput
@@ -59,6 +90,8 @@ const ScoutingPage = () => {
                               placeholder="Match"
                               autoComplete="off"
                               className="scouting-input"
+                              displayValue={(match: ShiftData | null) =>
+                                 match?.match.toString() ?? ""}
 
                            />
                            <AnimatePresence>
@@ -75,7 +108,14 @@ const ScoutingPage = () => {
                                  >
                                     <div id="scouting-dropdown-header">Matches</div>
                                     <div id="scouting-dropdown-line" />
-
+                                    {matches?.map((match) => (
+                                          <ComboboxOption
+                                             value={match}
+                                             id="scouting-dropdown-option"
+                                          >
+                                             {match.match}
+                                          </ComboboxOption>
+                                       ))}
                                  </ComboboxOptions>
                               )}
                            </AnimatePresence>
@@ -100,6 +140,8 @@ const ScoutingPage = () => {
                                  placeholder="Team"
                                  autoComplete="off"
                                  className="scouting-input"
+                                 displayValue={(match: ShiftData | null) =>
+                                    match?.team.toString() ?? ""}
 
                               />
                               <AnimatePresence>
@@ -116,7 +158,13 @@ const ScoutingPage = () => {
                                     >
                                        <div id="scouting-dropdown-header">Teams</div>
                                        <div id="scouting-dropdown-line" />
-
+                                       <ComboboxOption
+                                             //key={scouter.id}
+                                             value={selectedMatch?.team}
+                                             id="scouting-dropdown-option"
+                                          >
+                                             {selectedMatch?.team}
+                                          </ComboboxOption>
                                     </ComboboxOptions>
                                  )}
                               </AnimatePresence>
@@ -126,8 +174,11 @@ const ScoutingPage = () => {
                      </Combobox>
 
                   </div>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="54" height="54" viewBox="0 0 54 54" fill="none">
-                     <circle cx="27" cy="27" r="27" fill="#CDA745" fill-opacity="0.15" />
+                  <svg  
+                     id="scouting-svg"
+                     viewBox="0 0 54 54"
+                     > 
+                     <circle cx="27" cy="27" r="27" fill="#CDA745" fillOpacity="0.15" />
                   </svg>
                </Field>
             </div>
